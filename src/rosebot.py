13e16a15f1f6f7108a93bbee0ec2a64f21bea41e
hinojoat <hinojoat@rose-hulman.edu>
2019-02-06 14:43:20 -0500

    Changing Rosebot classes

--- src/rosebot.py
+++ src/rosebot.py
@@ -198,7 +198,6 @@ class ArmAndClaw(object):
                 break
         self.motor.turn_off()
 
-
     def calibrate_arm(self):
         """
         Calibrates its Arm, that is:
@@ -212,9 +211,11 @@ class ArmAndClaw(object):
         while True:
             if self.touch_sensor.is_pressed():
                 break
+        self.motor.turn_off()
+        self.motor.turn_on(-100)
         while abs(self.motor.get_position()) > 0:
             self.motor.turn_on(-100)
-
+            
     def move_arm_to_position(self, desired_arm_position):
         """
         Move its Arm to the given position, where 0 means all the way DOWN.
