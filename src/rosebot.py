f6a2139de877190647ab91c57b134774b5cd4241
hinojoat <hinojoat@rose-hulman.edu>
2019-02-07 14:59:21 -0500

    Adding the Handler functions in shared gui

--- src/rosebot.py
+++ src/rosebot.py
@@ -237,7 +237,7 @@ class ArmAndClaw(object):
         """
         self.motor.turn_on(-100)
         while True:
-            if abs(self.motor.get_position()) >= (14.2 * 360):
+            if abs(self.motor.get_position()) <= (0):
                 break
         self.motor.turn_off()
 
