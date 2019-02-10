5d756853ec57fd84a95a40c266de182c4ddeead9
hinojoat <hinojoat@rose-hulman.edu>
2019-02-10 15:41:12 -0500

    Updating the Rosebot

--- src/rosebot.py
+++ src/rosebot.py
@@ -87,20 +87,35 @@ class DriveSystem(object):
         Makes the robot go straight (forward if speed > 0, else backward)
         at the given speed for the given number of seconds.
         """
-
+        start = time.time()
+        self.go(speed, speed)
+        while True:
+            if time.time() - start >= seconds:
+                self.stop()
+                break
     def go_straight_for_inches_using_time(self, inches, speed):
         """
         Makes the robot go straight at the given speed
         for the given number of inches, using the approximate
         conversion factor of 10.0 inches per second at 100 (full) speed.
         """
-
+        
     def go_straight_for_inches_using_encoder(self, inches, speed):
         """
         Makes the robot go straight (forward if speed > 0, else backward)
         at the given speed for the given number of inches,
         using the encoder (degrees traveled sensor) built into the motors.
         """
+        inches_per_degree = self.wheel_circumference / 360
+        self.go(speed, speed)
+        self.left_motor.reset_position()
+        desired_degrees = inches / inches_per_degree
+        distance_gone = self.wheel_circumference / self.left_motor.get_position()
+        while True:
+            if distance_gone >= inches:
+                self.stop()
+                break
+
 
     # -------------------------------------------------------------------------
     # Methods for driving that use the color sensor.
